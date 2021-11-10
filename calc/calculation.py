bb87d1b7a21928ec4aa8b69ae212841b07fc7368
dhruvshah1996 <dbs28@njit.edu>
2021-11-10 15:11:43 -0500

    fixing calculation and addition class errors to make sure im fixing the correct errors

--- calc/calculation.py
+++ calc/calculation.py
@@ -2,8 +2,10 @@
 
 #This is the main calculation class
 class Calculation:
+
     # Constructor and it is the first function called when an object of the class is instantiated
     def __init__(self, value_a, value_b):
+
         # Constructor
         self.value_a = value_a
         self.value_b = value_b
