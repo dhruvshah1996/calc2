8fcc6047bc3a4673d5889224181d513325602522
dhruvshah1996 <dbs28@njit.edu>
2021-11-10 14:56:01 -0500

    Fixed Travis errors for Addition.py

--- calc/subtraction.py
+++ calc/subtraction.py
@@ -10,6 +10,6 @@ class Subtraction(Calculation):
     """This addition class has one method to get the result of the calculation A and B come from the
     calculation parent class"""
 
-    def getResult(self):
+    def getresult(self):
         #you need to use self to reference the data contained in the instance of the object. This is encapsulation
         return self.value_a - self.value_b
