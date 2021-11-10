21c02da8672bda922512bf9184899cb790cc99f7
dhruvshah1996 <dbs28@njit.edu>
2021-11-10 15:16:56 -0500

    fixing calculation and addition class errors to make sure im fixing the correct errors

--- calc/addition.py
+++ calc/addition.py
@@ -17,6 +17,7 @@ class Addition(Calculation):
 
     #This is the get result that is being called
     def getresult(self):
+
         #you need to use self to reference the data contained
         # in the instance of the object. This is encapsulation
         return self.value_a + self.value_b
