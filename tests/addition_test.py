4ae3f96b0e843baf3f5ea740bd2a9b82c57475eb
dhruvshah1996 <dbs28@njit.edu>
2021-12-04 15:21:03 -0500

    Finished Writing tests for Addition, Subtraction, and Multiplication

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,14 +1,12 @@
 """Testing Addition"""
-
 import pandas as pd
-
 from calc.calculations.addition import Addition
 
 def test_calculation_addition():
     """testing addition method with csv inputs"""
-    df = pd.read_csv("addition_csv.csv")
+    df = pd.read_csv("Addition.csv")
     print(df.head(5))
     for x, y in df.iterrows():
-        sum = (y.value_1, y.value_2)
+        sum = (y.Value_1, y.Value_2)
         addition = Addition.create(sum)
-        assert addition.get_result() == df['results'][x]
+        assert addition.get_result() == df['Result'][x]
